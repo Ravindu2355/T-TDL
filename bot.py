51f95013935672138dd6c1ee77df64ca57c2a010
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2025-02-20 10:25:59 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,21 @@
+import os
+import time
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import Config
+from globals import AuthU
+from plugins.authers import is_authorized
+
+
+# Environment variables
+API_ID = Config.API_ID
+API_HASH = Config.API_HASH
+BOT_TOKEN = Config.BOT_TOKEN
+OWNER = Config.OWNER
+
+# Initialize Pyrogram client
+plugins = dict(root="plugins")
+app = Client("te_downloader_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN,plugins=plugins)
+
+# Run the bot
+app.run()
